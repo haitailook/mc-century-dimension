84e1bbd9549326fae4c5bc319b683f728369db4b
htawa <haitailook@outlook.com>
2024-11-29 23:08:18 +0800

    new mod

---
+++ src/main/java/cn/tlgjx/centurydimension/CenturyDimensionDataGenerator.java
@@ -0,0 +1,11 @@
+package cn.tlgjx.centurydimension;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CenturyDimensionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
